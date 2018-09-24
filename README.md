5dbc065a58ccc72def1eab7732d4a81a752fe4bd
Marcin Wojciechowski <marcin.wojciechowski2@gmail.com>
2018-09-24 22:17:35 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# syn_touchpad_toggle
+
+Simple utility which toggles Synaptics touchpad on/off whenever you need it :)
+Just bind it with your favorite key
---
+++ touchpad.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+timeout=2000
+
+check_state() {
+   synclient  | awk -F= '{ gsub(/[ \t]+/, ""); if ($1 == "TouchpadOff") print $2; }'
+}
+
+case "${1}" in
+[oO][nN])
+	t=0
+	msg=${1}
+	;;
+[oO][fF][fF])
+	t=1
+	msg=${1}
+	;;
+--help)
+	echo "Usage: ${0} [on|off]"
+	exit 0
+	;;
+*)
+	t=$(( ! $(check_state) ))
+	(( t == 0 )) && msg='ON' || msg='OFF'
+	;;
+esac
+
+synclient TouchpadOff=${t}
+notify-send -t "${timeout}" 'Touchpad' "${msg^^}"
